88062b8a45dbd876d9cda9d6ccce59284a113fa1
Erica! <eannguyen@ucdavis.edu>
2020-12-29 20:01:33 -0800

    [Re: e2nguyen/Watcher#23]
    - renamed components test page to be components playground since test in naming conventions should be reserved for actual tests
       - additional renamed function component to be ComponentsPlayground fomr Playground because exported components should match their file name
    - added sample character sprites for our example custom image component to use
    - added custom image component using next.js's image component

---
+++ pages/components-playground.tsx
@@ -0,0 +1,21 @@
+import CustomImageComponent from 'components/CustomImageComponent';
+
+function ComponentsPlayground() {
+  return(
+    <div>
+      <h1>PLAYGROUND</h1>
+      <div className="custom-image-component-playground-container">
+        <h3>Custom Image Component</h3>
+        <CustomImageComponent
+          alt="sprites"
+          className="custom-image-component-example"
+          height="450"
+          src="/character-sprites-sample.png"
+          width="750"
+        />
+      </div>  
+    </div>
+  );
+};
+
+export default ComponentsPlayground;
