48e898b01dc0c839a3b5158231f98f8bb9aa3886
Erica! <eannguyen@ucdavis.edu>
2021-01-16 11:37:42 -0800

    [Re: e2nguyen/Watcher#23] added styling to the component's playground heading 1 to center align title

--- pages/components-playground.tsx
+++ pages/components-playground.tsx
@@ -2,19 +2,28 @@ import CustomImageComponent from 'components/CustomImageComponent';
 
 function ComponentsPlayground() {
   return(
-    <div>
-      <h1>PLAYGROUND</h1>
-      <div className="custom-image-component-playground-container">
-        <h3>Custom Image Component</h3>
-        <CustomImageComponent
-          alt="sprites"
-          className="custom-image-component-example"
-          height="450"
-          src="/character-sprites-sample.png"
-          width="750"
-        />
-      </div>  
-    </div>
+    <>
+      <style jsx>
+        {`
+          h1 {
+            text-align: center;
+          }
+        `}
+      </style>
+      <div>
+        <h1>PLAYGROUND</h1>
+        <div className="custom-image-component-playground-container">
+          <h3>Custom Image Component</h3>
+          <CustomImageComponent
+            alt="sprites"
+            className="custom-image-component-example"
+            height="450"
+            src="/character-sprites-sample.png"
+            width="750"
+          />
+        </div>  
+      </div>
+    </>
   );
 };
 
