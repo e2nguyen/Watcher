68b49e851b76929edc01247e4154995f04ad9eda
dIANE phan <dianephan12@gmail.com>
2021-02-05 21:40:56 -0800

    [Re: e2nguyen/Watcher#24] created a CustomDivComponent and main-experience pg

---
+++ components/CustomDivComponent.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+interface Props {
+  className: string;
+  children: React.ReactNode;
+  color: string;
+  header: string;
+  height: string;
+  width: string;
+}
+
+const CustomDivComponent: React.FC<Props> = ({ 
+  className, 
+  children, 
+  color,
+  header,
+  height,
+  width
+}) => {
+  return (
+    <>
+    <div className = {className}>
+        <h1>{header}</h1>
+      <div className = "body">{children}</div>
+    </div>
+    <style jsx>{`
+        .${className}{
+          background-color: ${color};
+          height: ${height};
+          width: ${width}
+        }
+      `}</style>
+    </>
+  )
+};
+
+export default CustomDivComponent;
+  
+
+
+{
+/* 
+const wrapper = () =>  {
+	const [update] = useMutation()
+
+onhover(
+  update(+1);
+)
+
+<div className=”wrapper”>
+   		{children}
+</div>
+}
+*/
+}
--- pages/components-playground.tsx
+++ pages/components-playground.tsx
@@ -1,19 +1,9 @@
 import Button from "components/button/button"
+import CustomDivComponent from "components/CustomDivComponent"
 
 function Playground() {
   return ( <>
-    <Button onClick={() => console.log("you clicked the cat!")}
-    backgroundURL = "/cat.png"
-    height = "200px"
-    width = "200px"
-    > 
-     </Button>
-    <Button onClick={() => console.log("you clicked me")}
-    backgroundURL = "/girl-pink.png"
-    height = "200px"
-    width = "200px"
-    > 
-      </Button>
+   empty
   </> )
 }
 
