884e702582766ae65a6ce510cea3abc69a0810ab
dIANE phan <dianephan12@gmail.com>
2021-02-20 13:53:20 -0800

    [Re: e2nguyen/Watcher#37] Wrapped the CustomImageComponent with new AvatarComponent and made basic styling changes

---
+++ components/AvatarComponent.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Headline from "components/Headline"
+import CustomImageComponent from "components/CustomImageComponent";
+
+interface Props {
+  alt: string;
+  children: React.ReactNode;
+  className: string;
+  imageClassName: string;
+  src: string;
+}
+
+const AvatarStyling: React.FC<{className: string}> = ({className}) => (
+  <style jsx global>{`
+    .${className} {
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  `}</style>
+)
+
+const AvatarComponent: React.FC<Props> = ({
+  alt,
+  children,
+  className,
+  imageClassName,
+  src,
+}) => (
+  <div className={`flex-container ${className}`}>
+    <CustomImageComponent 
+      alt={alt}
+      className={imageClassName}
+      height="50px"
+      src={src}
+      width="50px"
+    >
+      <AvatarStyling className={imageClassName}/>
+    </CustomImageComponent>
+    {children}
+    <style jsx global>{`
+      .${className} {
+        color: pink;
+        height: 60px;
+        vertical-align: middle;
+        display: flex;
+        align-items: center;
+      }
+      p {
+        display: flex;
+        align-items: center;
+        height: 50px;
+        margin: 0;
+        padding-left: 20px;
+        vertical-align: middle;
+        width: 300px;
+      }
+    `}</style>
+  </div>
+);
+
+export default AvatarComponent;
+  
