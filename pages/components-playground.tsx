a925d4a4bd56270e7461ab0cf3241a142dd3e822
Erica! <eannguyen@ucdavis.edu>
2021-03-06 11:47:48 -0800

    [Re: e2nguyen/Watcher#41] - created like button with simple console logging onclick - updated button exports by exporting buttons as default from index files for easy access elsewhere - added like button to the playground for QA

--- pages/components-playground.tsx
+++ pages/components-playground.tsx
@@ -1,24 +1,35 @@
-import Button from "components/button/button"
 import Headline from "components/Headline"
+import { Button, LikeButton } from "components/button";
+import CustomImageComponent from "components/CustomImageComponent"
 
 function Playground() {
   return (
     <>
       <Button 
         onClick={() => console.log("you clicked the cat!")}
+        backgroundSize="200px"
         backgroundURL = "/cat.png"
-        height = "200px"
-        width = "200px"
+        height="200px"
+        width="200px"
       /> 
       <Button 
         onClick={() => console.log("you clicked me")}
-        backgroundURL = "/girl-pink.png"
-        height = "200px"
-        width = "200px"
+        backgroundSize="200px"
+        backgroundURL="/girl-pink.png"
+        height="200px"
+        width="200px"
       />
       <Headline>
         This is a Headline 
       </Headline>
+      <CustomImageComponent
+        alt="bleh"
+        className="meh"
+        height="200px"
+        src="/character-sprites-sample.png"
+        width="320px"
+      />
+      <LikeButton/>
     </>
   );
 }
