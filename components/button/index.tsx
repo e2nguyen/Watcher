20f81b1b7807a3b278317f8125802b429799608f
dIANE phan <dianephan12@gmail.com>
2021-05-01 11:57:42 -0700

    lowercase button folder name

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,5 +1,5 @@
 export { default as BookmarkButton } from './BookmarkButton';
-export { default as Button } from './Button';
+export { default as Button } from './button';
 export { default as CommentButton } from './CommentButton';
 export { default as LikeButton } from './LikeButton';
 export { default as ShareButton } from './ShareButton';
