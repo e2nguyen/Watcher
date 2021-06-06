1b272e266b5b0a39cca9ba3d60023152b4b7feef
dIANE phan <dianephan12@gmail.com>
2021-06-06 11:12:25 -0700

    [Re: 62] added backgroundImages and backgroundSizes to use for soc. media buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ['./pages/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './components/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'heart-icon': "url('/heart-icon.png')",
+        'plane-share': "url('/plane-share.png')",
+        'pencil-comment': "url('/pencil-comment.png')",
+        'bookmark': "url('/bookmark.png')",
+      })
+    },
+    backgroundSize: {
+      'auto': 'auto',
+      'cover': 'cover',
+      'contain': 'contain',
+      '50%': '50%',
+      'size-18': '18px', // TODO: change px to rem
+    },
   },
   variants: {
     extend: {},
