2f1ad42fb9fbc37f7ae4cf8913b94ae1a107769a
dIANE phan <dianephan12@gmail.com>
2021-11-06 11:18:02 -0700

    [Re: #71] : moved the ApolloClient into the ApolloWrapper otherwise provider does not recognize the client

--- components/ApolloWrapper.tsx
+++ components/ApolloWrapper.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import client from "../ApolloClient";
 import { ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+
 
 const ApolloWrapper = ({children}: {children: React.ReactNode}
   ): JSX.Element => {
+
+    const client = new ApolloClient({
+      uri: "https://graphql.us.fauna.com/graphql",
+      headers: {
+        authorization: 'Bearer fnAEVMSosTAASA-uV48zyykFYJ3wsuU_RebTzT6p',
+      },
+      cache: new InMemoryCache(),
+    });
+
     return (
       <ApolloProvider client={client}>
         {children}
       </ApolloProvider>
     )
+
 }
 
 export default ApolloWrapper;
